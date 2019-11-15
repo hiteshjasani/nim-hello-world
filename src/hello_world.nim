c34f807d7f1d3edff4c4785044a00ca721d0cb44
Hitesh Jasani <hitesh.jasani@gmail.com>
2019-11-14 23:09:40 -0500

    kickoff new build

--- src/hello_world.nim
+++ src/hello_world.nim
@@ -1,5 +1,4 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+const msg = "Hello, World!"
 
 when isMainModule:
-  echo("Hello, World!")
+  echo msg
